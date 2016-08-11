5175d13e2fcd0391798f98cdb3f11059c866775a
Lorn Potter <lorn.potter@canonical.com>
2016-08-11 16:48:22 +1000

    fix up licensing

--- sensehatcompass.cpp
+++ sensehatcompass.cpp
@@ -1,3 +1,34 @@
+//Copyright 2015- Raspberry Pi Foundation
+
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions are met:
+
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+
+//    * Neither the name of the copyright holder nor the
+//      names of its contributors may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
+
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//POSSIBILITY OF SUCH DAMAGE.
+
+//* Copyright (C) 2016 Canonical Ltd
+
+
 #include <QCoreApplication>
 #include <QLoggingCategory>
 #include <QtGui/QPainter>
@@ -6,9 +37,11 @@
 
 #include "sensehatcompass.h"
 #include "qsensehatfb.h"
-#define LED_LOOP = [4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8, 0, 1, 2, 3]
-//#define LED_DEGREE_RATIO = sizeof
-SensehatCompass::SensehatCompass(QObject *parent) : QObject(parent)
+
+// parts from compass.py example from python-sense-hat
+
+SensehatCompass::SensehatCompass(QObject *parent)
+    : QObject(parent)
 {
     ledLoop = {4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8, 0, 1, 2, 3};
 
@@ -35,7 +68,7 @@ void SensehatCompass::compassChanged()
 {
   //  qreal level = compass->reading()->calibrationLevel();
     qreal direction = compass->reading()->azimuth();
-    qDebug() << Q_FUNC_INFO << direction;
+//    qDebug() << Q_FUNC_INFO << direction;
     int directionInverted = 360 - direction;
     int index = int(ledDegreeRatio * directionInverted);
     int offset = ledLoop[index];
